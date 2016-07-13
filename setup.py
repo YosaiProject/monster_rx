cb5b60bc922c392ffcb2553230a067a826d1b1f2
dowwie <dkcdkg@gmail.com>
2016-07-13 09:04:50 -0400

    updated setup

--- setup.py
+++ setup.py
@@ -20,32 +20,22 @@ requires = [
     'yosai_alchemystore'
     ]
 
-tests_require = [
-    'WebTest >= 1.3.1',  # py3 compat
-    'pytest',  # includes virtualenv
-    'pytest-cov',
-    ]
-
 setup(name='monster_rx',
       version='0.0',
       description='monster_rx',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
           "Programming Language :: Python",
-          "Framework :: Pyramid",
           "Topic :: Internet :: WWW/HTTP",
           "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
       ],
       author='',
       author_email='',
       url='',
-      keywords='web wsgi bfg pylons pyramid',
+      keywords='web pyramid yosai',
       packages=find_packages(),
       include_package_data=True,
       zip_safe=False,
-      extras_require={
-          'testing': tests_require,
-      },
       install_requires=requires,
       entry_points="""\
       [paste.app_factory]
